6c63209f01418eda92b6543f88920903bb9b65f3
Brian <brian@opengamma.com>
2019-04-03 12:17:22 +0100

    Interview changes

--- modules/product/src/main/java/com/opengamma/strata/product/common/PayReceive.java
+++ modules/product/src/main/java/com/opengamma/strata/product/common/PayReceive.java
@@ -5,12 +5,6 @@
  */
 package com.opengamma.strata.product.common;
 
-import org.joda.convert.FromString;
-import org.joda.convert.ToString;
-
-import com.opengamma.strata.collect.named.EnumNames;
-import com.opengamma.strata.collect.named.NamedEnum;
-
 /**
  * Flag indicating whether a financial instrument is "pay" or "receive".
  * <p>
@@ -18,7 +12,7 @@ import com.opengamma.strata.collect.named.NamedEnum;
  * For example, a swap typically has two legs, a pay leg, where payments are made
  * to the counterparty, and a receive leg, where payments are received.
  */
-public enum PayReceive implements NamedEnum {
+public enum PayReceive {
 
   /**
    * Pay.
@@ -28,95 +22,4 @@ public enum PayReceive implements NamedEnum {
    * Receive.
    */
   RECEIVE;
-
-  // helper for name conversions
-  private static final EnumNames<PayReceive> NAMES = EnumNames.of(PayReceive.class);
-
-  //-------------------------------------------------------------------------
-  /**
-   * Obtains an instance from the specified name.
-   * <p>
-   * Parsing handles the mixed case form produced by {@link #toString()} and
-   * the upper and lower case variants of the enum constant name.
-   * 
-   * @param name  the name to parse
-   * @return the type
-   * @throws IllegalArgumentException if the name is not known
-   */
-  @FromString
-  public static PayReceive of(String name) {
-    return NAMES.parse(name);
-  }
-
-  /**
-   * Converts a boolean "is pay" flag to the enum value.
-   * 
-   * @param isPay  the pay flag, true for pay, false for receive
-   * @return the equivalent enum
-   */
-  public static PayReceive ofPay(boolean isPay) {
-    return isPay ? PAY : RECEIVE;
-  }
-
-  /**
-   * Converts a signed amount to the enum value.
-   * <p>
-   * A negative value will return 'Pay'.
-   * A positive value will return 'Receive'.
-   * This effectively parses the result of {@link #normalize(double)}.
-   * 
-   * @param amount  the amount to check
-   * @return the equivalent enum
-   */
-  public static PayReceive ofSignedAmount(double amount) {
-    return Double.compare(amount, 0d) < 0 ? PAY : RECEIVE;
-  }
-
-  //-------------------------------------------------------------------------
-  /**
-   * Normalizes the specified notional amount using this pay/receive rule.
-   * <p>
-   * This returns a negative signed amount if this is 'Pay', and a positive
-   * signed amount if this is 'Receive'. This effectively normalizes the input
-   * notional to the pay/receive sign conventions of this library.
-   * 
-   * @param amount  the amount to adjust
-   * @return the adjusted amount
-   */
-  public double normalize(double amount) {
-    double normalized = Math.abs(amount);
-    return isPay() ? -normalized : normalized;
-  }
-
-  //-------------------------------------------------------------------------
-  /**
-   * Checks if the type is 'Pay'.
-   * 
-   * @return true if pay, false if receive
-   */
-  public boolean isPay() {
-    return this == PAY;
-  }
-
-  /**
-   * Checks if the type is 'Receive'.
-   * 
-   * @return true if receive, false if pay
-   */
-  public boolean isReceive() {
-    return this == RECEIVE;
-  }
-
-  //-------------------------------------------------------------------------
-  /**
-   * Returns the formatted name of the type.
-   * 
-   * @return the formatted string representing the type
-   */
-  @ToString
-  @Override
-  public String toString() {
-    return NAMES.format(this);
-  }
-
 }
--- modules/product/src/main/java/com/opengamma/strata/product/swap/SwapLegType.java
+++ modules/product/src/main/java/com/opengamma/strata/product/swap/SwapLegType.java
@@ -5,19 +5,13 @@
  */
 package com.opengamma.strata.product.swap;
 
-import org.joda.convert.FromString;
-import org.joda.convert.ToString;
-
-import com.opengamma.strata.collect.named.EnumNames;
-import com.opengamma.strata.collect.named.NamedEnum;
-
 /**
  * The type of a swap leg.
  * <p>
  * This provides a high-level categorization of a swap leg.
  * This is useful when it is necessary to find a specific leg.
  */
-public enum SwapLegType implements NamedEnum {
+public enum SwapLegType {
 
   /**
    * A fixed rate swap leg.
@@ -49,55 +43,5 @@ public enum SwapLegType implements NamedEnum {
    * A swap leg that is not based on a Fixed, Ibor, Overnight or Inflation rate.
    */
   OTHER;
-
-  // helper for name conversions
-  private static final EnumNames<SwapLegType> NAMES = EnumNames.of(SwapLegType.class);
-
-  //-------------------------------------------------------------------------
-  /**
-   * Obtains an instance from the specified name.
-   * <p>
-   * Parsing handles the mixed case form produced by {@link #toString()} and
-   * the upper and lower case variants of the enum constant name.
-   * 
-   * @param name  the name to parse
-   * @return the type
-   * @throws IllegalArgumentException if the name is not known
-   */
-  @FromString
-  public static SwapLegType of(String name) {
-    return NAMES.parse(name);
-  }
-
-  //-------------------------------------------------------------------------
-  /**
-   * Checks if the type is 'Fixed'.
-   * 
-   * @return true if fixed, false otherwise
-   */
-  public boolean isFixed() {
-    return this == FIXED;
-  }
-
-  /**
-   * Checks if the type is floating, defined as 'Ibor', 'Overnight' or 'Inflation'.
-   * 
-   * @return true if floating, false otherwise
-   */
-  public boolean isFloat() {
-    return this == IBOR || this == OVERNIGHT || this == INFLATION;
-  }
-
-  //-------------------------------------------------------------------------
-  /**
-   * Returns the formatted name of the type.
-   * 
-   * @return the formatted string representing the type
-   */
-  @ToString
-  @Override
-  public String toString() {
-    return NAMES.format(this);
-  }
 
 }
